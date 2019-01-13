1c0bc3c94edb4a01656e7c7312cf9ec88ef6d0a1
griz <grizzly.anderson@gmail.com>
2019-01-13 09:45:38 -0700

    notes to asset generator

--- assets_generate.go
+++ assets_generate.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 )
 
+// Intended to be used with `go generate` to create ngram data files
 func main() {
+	// source or and information about ngamrs: http://practicalcryptography.com/cryptanalysis/text-characterisation/quadgrams/
 	var fs http.FileSystem = http.Dir("data/english_quadgrams.txt")
 
 	err := vfsgen.Generate(fs, vfsgen.Options{PackageName: "data", VariableName: "QuadGrams", Filename: "data/eng_quadgrams.go"})
